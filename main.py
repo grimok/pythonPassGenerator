931ef7b0db5f960deb5a350e69c87c9e18e3198b
Yiğit YILMAZ <83428666+grimthedeveloper@users.noreply.github.com>
2022-09-16 13:39:08 +0000

    Added telephone sms message system template and beautified code

--- main.py
+++ main.py
@@ -1,27 +1,35 @@
 import random
+import time
 
-lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+lowercase = [
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
+    'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+]
 
-uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+uppercase = [
+    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O',
+    'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+]
 
-symbols = ['@', '#','!','~','$','%','^',
-                '&','*','(',',','-','+','/',
-                ':','.',',','<','>','?','|']
+symbols = [
+    '@', '#', '!', '~', '$', '%', '^', '&', '*', '(', ',', '-', '+', '/', ':',
+    '.', ',', '<', '>', '?', '|'
+]
 
 password = random.choice(lowercase)
 
-for i in range(1,12):
-  if(i < 8):
-    t = random.randint(1,2)
-    if(t == 2):
-      password += random.choice(lowercase)
+for i in range(1, 12):
+    if (i < 8):
+        t = random.randint(1, 2)
+        if (t == 2):
+            password += random.choice(lowercase)
+        else:
+            password += random.choice(uppercase)
     else:
-      password += random.choice(uppercase)
-  else:
-    password += random.choice(symbols)
+        password += random.choice(symbols)
 
-for x in range(1,5):
-  password += str(random.randint(1,9))
+for x in range(1, 5):
+    password += str(random.randint(1, 9))
 
 #print(password)
 
@@ -30,6 +38,21 @@ random.shuffle(l)
 password = ''
 
 for x in l:
-  password += x
+    password += x
 
-print(password)
+print(f'Generated Password: {password}')
+
+while (True):
+    inp = str(
+        input('Do you want me to send this password on your phone? (Y/N)'))
+    if inp.upper() == 'Y':
+        print('Your password has sucessfuly sent!')
+        break
+    elif inp.upper() == 'N':
+        print('Skipping !')
+        break
+    else:
+        print('Unknown input type! Please answer with Y or N.')
+
+print('Sistemden çıkılıyor...')
+time.sleep(5)
